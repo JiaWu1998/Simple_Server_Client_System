23c1d9ba30b2d3dca9a5dea9052e8478522f3e91
James <Jiawu1998@github.com>
2021-03-01 21:04:42 -0600

    incoming deployment file

--- deployment/deployment.py
+++ deployment/deployment.py
@@ -1,1 +1,51 @@
-asdiuh
+import os
+import shutil
+
+# Get parent directory
+PARENT_DIR = os.path.dirname(os.path.abspath(__file__))
+N = 1
+
+# Create server directory
+def create_server():
+    if not os.path.exists(f"{PARENT_DIR}/../server"):
+        os.mkdir(f"{PARENT_DIR}/../server")
+    if not os.path.exists(f"{PARENT_DIR}/../server/watch_folder"):
+        os.mkdir(f"{PARENT_DIR}/../server/watch_folder")
+
+    shutil.copyfile(f"{PARENT_DIR}/config.json", f"{PARENT_DIR}/../server/config.json")
+    shutil.copyfile(f"{PARENT_DIR}/server.py", f"{PARENT_DIR}/../server/server.py")
+
+# Create N client directories
+def create_clients():
+    for i in range(N):
+        if not os.path.exists(f"{PARENT_DIR}/../client_{i}"):
+            os.mkdir(f"{PARENT_DIR}/../client_{i}")
+        if not os.path.exists(f"{PARENT_DIR}/../client_{i}/download_folder"):
+            os.mkdir(f"{PARENT_DIR}/../client_{i}/download_folder")
+
+        shutil.copyfile(f"{PARENT_DIR}/config.json", f"{PARENT_DIR}/../client_{i}/config.json")
+        shutil.copyfile(f"{PARENT_DIR}/client.py", f"{PARENT_DIR}/../client_{i}/client.py")
+
+# Delete server directory
+def delete_server():
+    shutil.rmtree(f"{PARENT_DIR}/../server")
+
+# Delete N client directories
+def delete_clients():
+    for i in range(N):
+        shutil.rmtree(f"{PARENT_DIR}/../client_{i}")
+
+if __name__ == "__main__":
+    
+    try:
+        N = int(input("Number of clients: "))
+        
+        if N <= 0:
+            print("Input must be 1 or higher.")
+    except:
+        print("Input is not a number.")
+    
+    create_server()
+    create_clients()
+    delete_server()
+    delete_clients()
